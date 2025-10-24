15507b9a03462a1728b2b29d639591fdd239abaf
Keshia Adrin <keshady26.bae@gmail.com>
2025-10-24 20:23:41 +0530

    Basic Structure

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+function MovieCard({movie}) {
+    const onFavClick = () => {
+        alert("clicked")
+    }
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favourite-btn" onClick={() => {onFavClick()}}>💗</button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3 className="movie-title">{movie.title}</h3>
+                <p>{movie.releaseDate}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
